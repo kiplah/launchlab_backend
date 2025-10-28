05d44aee0355bd4602a71cc0f61ecb6edd5a8329
kiplah <kiplahvictor27@gmail.com>
2025-10-28 20:05:45 +0300

    Add ReviewsConfig and initial admin styles; configure admin sidebar

--- reviews/apps.py
+++ reviews/apps.py
@@ -1,6 +1,9 @@
 from django.apps import AppConfig
 
-
 class ReviewsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'reviews'
+
+    def ready(self):
+        from django.contrib import admin
+        admin.site.enable_nav_sidebar = False
